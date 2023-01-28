03aeb05cff63aa475f27544595b2966d176e5ebb
unknown <alexbaukartjarschel@gmail.com>
2023-01-28 09:24:56 -0300

    estrutura de repetição

---
+++ logica dart/repeticao.dart
@@ -0,0 +1,14 @@
+void main() {
+  //estruturas de repetição
+
+  for (int i = 1; i <= 10; i++) {
+    int resultado = i * 2;
+    print("2x$i = $resultado");
+  }
+
+  int contador = 10;
+  while (contador != 1) {
+    print(contador);
+    contador = contador - 1;
+  }
+}
